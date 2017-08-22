9313f58fb9c8ac12bf537ce69e87bdd8666b8145
Timothy Cheung <timothycheung80@yahoo.com>
2017-08-22 09:29:41 -0700

    Timothy Cheung

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+
+//Tim Cheung
+//Project description:my second project, compare println and print
+// Date: August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+	System.out.println("hello, this is project 2!!");
+
+	}
+
+}
