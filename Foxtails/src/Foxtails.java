943d030cd808c4f22923de94b9935363dff1233b
Timchung18 <31105410+Timchung18@users.noreply.github.com>
2017-08-28 18:03:26 -0700

    random stuff

---
+++ Foxtails/src/Foxtails.java
@@ -0,0 +1,11 @@
+//hello this is timothy cheung
+public class Foxtails {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, my name is Timothy!");
+		System.out.println("\t\"\n\\-_");
+		System.out.print("foxtails$");
+
+	}
+
+}
