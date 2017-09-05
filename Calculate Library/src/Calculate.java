b93d72ccb9ff90c3f0c9bbecc6b7a9803bccd527
Timchung18 <31105410+Timchung18@users.noreply.github.com>
2017-09-05 14:24:35 -0700

    Add files via upload

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,25 @@
+/* Tim Cheung
+ * September 5, 2017
+ * this library contains several mathematical methods and calculations
+ */
+public class Calculate {
+	//This method takes an integer and returns its square
+	
+	//This method takes an integer and returns its cube
+	public static int cube (int operand) {
+			return operand * operand * operand;
+	}
+	//This method averages two numbers 
+	public static double average(double value1, double value2) {
+		return (value1 + value2) / 2;
+	}
+	//This method averages three numbers (overloading)
+	public static double average(double value1, double value2, double value3) {
+		return (value1 + value2 + value3) / 3;
+	}
+	//This method converts an angle measure given in degrees into an angle in radians
+	public static double toRadians(double angle_in_degrees) {
+		return angle_in_degrees * (3.14159 / 180);
+	}
+//THis method
+}
