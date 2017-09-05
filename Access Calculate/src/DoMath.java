6ee18167e941921661c2fe9f169598e48078ceb6
Timchung18 <31105410+Timchung18@users.noreply.github.com>
2017-09-05 14:50:13 -0700

    Add files via upload

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+/* Tim Cheung
+ * September 5, 2017
+ * This is the runner class for my Calculate Library methods
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-5));
+		System.out.println(Calculate.cube(3));
+		System.out.println(Calculate.average(2.6,2.7));
+		System.out.println(Calculate.average(2.0,3.0,4.0));
+		
+	}
+
+}
