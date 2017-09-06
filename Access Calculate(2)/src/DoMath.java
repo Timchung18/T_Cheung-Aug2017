0b3fec5b7550c2109f7ab29ecdf2e077507284cf
Timothy Cheung <timothycheung80@yahoo.com>
2017-09-06 11:25:02 -0700

    wednesday commit flex

--- Access Calculate(2)/src/DoMath.java
+++ Access Calculate(2)/src/DoMath.java
@@ -5,11 +5,9 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		System.out.println(Calculate.square(-5));
+		System.out.println(Calculate.square(-2));
 		System.out.println(Calculate.cube(3));
 		System.out.println(Calculate.average(2.6,2.7));
 		System.out.println(Calculate.average(2.0,3.0,4.0));
-		
-	}
-
+}
 }
