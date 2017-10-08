bd41247dd7a7c4a88457aad83b29775579519591
Timchung18 <31105410+Timchung18@users.noreply.github.com>
2017-10-07 20:40:49 -0700

    editted

--- ProccessingNumbers/src/ProcessingNumbers.java
+++ ProccessingNumbers/src/ProcessingNumbers.java
@@ -4,8 +4,6 @@ public class ProcessingNumbers {
 	public static void main(String[] args) {
 		int number;
 		int sum = 0;
-		int greatestnum = 0;
-		int leastnum = 0;
 		String yes = "yes";
 		@SuppressWarnings("resource")
 		Scanner userinput = new Scanner(System.in);
@@ -14,8 +12,8 @@ public class ProcessingNumbers {
 		if (number%2 == 0) {
 			sum = number + sum;
 		}
-		greatestnum = number;
-		leastnum = number;
+		int greatestnum = number;
+		int leastnum = number;
 		System.out.println("Do you want to enter another number?");
 		String keepgoing = userinput.next();
 		while (keepgoing.equals(yes)) {
