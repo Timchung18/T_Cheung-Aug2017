ffe05700a6e3f45c8d1bf9b2861ee4b6d2537c8a
Timothy Cheung <timothycheung80@yahoo.com>
2017-11-28 09:20:08 -0800

    code magnet/drums in class activity

---
+++ CodeMagnets/src/DrumKit.java
@@ -0,0 +1,12 @@
+
+public class DrumKit {
+	boolean topHat = true;
+	boolean snare = false;
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
+	}
+
+}
