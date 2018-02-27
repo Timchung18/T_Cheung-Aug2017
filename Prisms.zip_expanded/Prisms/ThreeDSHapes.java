2a9d39d269a75d76b7e43f7c59c31f02633e9592
Timothy Cheung <timothycheung80@yahoo.com>
2018-02-27 08:20:25 -0800

    three d shape

---
+++ Prisms.zip_expanded/Prisms/ThreeDSHapes.java
@@ -0,0 +1,23 @@
+
+public class ThreeDSHapes {
+	private double volume;
+	private double surfaceArea;
+
+	public ThreeDSHapes()
+	{
+	this.volume = calcVolume();
+	this.surfaceArea = calcSA();
+	}
+
+	// Can you write this code for this class or does it need to be overridden by every subclass?
+	public double calcVolume()
+	{
+	return 0;
+	}
+
+	// Can you write this code for this class or does it need to be overridden by every subclass?
+	public double calcSA()
+	{
+	return 0;
+	}
+}
